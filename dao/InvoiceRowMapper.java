98af53a5873e70d88dea4b090e12ed36c20a0885
Andreea <36197919+al361923@users.noreply.github.com>
2020-03-03 02:17:39 +0100

    Add files via upload
    Añadidas las clases RowMapper (y primer intento de commit)

---
+++ dao/InvoiceRowMapper.java
@@ -0,0 +1,23 @@
+package es.uji.ei1027.majorsacasa.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.jdbc.core.RowMapper;
+import es.uji.ei1027.majorsacasa.model.Invoice;
+
+public final class InvoiceRowMapper implements RowMapper<Invoice> {
+	public Invoice mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Invoice invoice = new Invoice();
+		invoice.setDate(rs.getDate("date"));
+		invoice.setNumber(rs.getInt("number"));
+		invoice.setAmount(rs.getFloat("amount"));
+		invoice.setConcept(rs.getString("concept"));
+		invoice.setElderly_dni("elderly_dni");
+		return invoice;
+	}
+}
+
+
+
---
+++ dao/PayRowMapper.java
@@ -0,0 +1,19 @@
+package es.uji.ei1027.majorsacasa.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.jdbc.core.RowMapper;
+import es.uji.ei1027.majorsacasa.model.Pay;
+
+public final class PayRowMapper implements RowMapper<Pay>{
+	public Pay mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Pay pay = new Pay();
+		pay.setRequest_number(rs.getInt("request_number"));
+		pay.setInvoice_number(rs.getInt("invoice_number"));
+		return pay;
+		
+	}
+
+}
